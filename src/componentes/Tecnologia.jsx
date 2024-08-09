1fad887bbcebc6c57db04726ba115e09c3e54e7d
MarlonLupaca <marlondiegomendoza@gmail.com>
2024-08-08 23:15:21 -0500

    Seccion del sobre mi creado

--- src/componentes/Tecnologia.jsx
+++ src/componentes/Tecnologia.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Tecnologia = ({name_url, name}) => {
     return (
         <div className='flex flex-col items-center gap-1 hover:scale-[1.08] transition-all' >
-            <img src={`/Portfolio_dev/src/img/${name_url}`} alt={name_url} className='h-8 sm:h-10 md:h-[70px]'/>
+            <img src={`/Portfolio_dev/assets/img/${name_url}`} alt={name_url} className='h-8 sm:h-10 md:h-[70px]'/>
             <p className='text-[10px] mt-1 md:text-[16px]'>{name}</p>
         </div>
     )
