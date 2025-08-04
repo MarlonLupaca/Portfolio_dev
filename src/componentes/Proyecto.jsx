ccb0f92633a8e106f77c2784ed72aa5af508bdfa
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-08-04 18:00:53 -0500

    Se creo un nuevo proyecto FullStack

--- src/componentes/Proyecto.jsx
+++ src/componentes/Proyecto.jsx
@@ -1,49 +1,51 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 const Proyecto = ({ proyecto }) => {
-    
-    
-    return (
-        <div className="w-[325PX] sm:w-[350PX] bg-[rgba(255,255,255,0.1)] flex flex-col items-center px-6 justify-around py-4 pb-3 rounded-lg mt-2 mb-[30px] md:mb-0 " >
-
-            <p className="font-[800] text-[20px] mb-2">{proyecto.Nombre.primero} <span className="text-[#FF69B4]">{proyecto.Nombre.segundo}</span></p>
-            <div className="relative">
-                <div className="w-full h-[101%] flex items-end filtrado absolute rounded-lg">
-                    <div className="h-fit py-[10px] px-3 w-full flex gap-3 ">
-                        {
-                        proyecto.Tecnologias.map((item, index) => (
-                            <div key={index} className="bg-gray-600 w-fit font-[800] text-[12px] py-1 px-2 rounded-lg">
-                            {item}
-                            </div>
-                        ))
-                        }
-
-                    </div>
-                    
-                </div>
-                <img src={proyecto.url.imagen} alt="" className="w-full rounded-lg"/>
-            </div>
-            
-            <p className="text-[13px] mt-[20px] mb-[15px] font-[300] my-2 text-pretty">{proyecto.Descripcion}</p>
-            <div className="flex gap-5 w-full justify-end mb-2">
-                {
-                    proyecto.Vistas.github && <a href={proyecto.url.github}
-                    className="text-[13px] h-8 px-3 py-2 rounded-lg font-[800] bg-black flex items-center gap-2 hover:shadow-custom-white transition-all" target="_blank" rel="noopener noreferrer">
-                        <span>Github</span>
-                        <img src="img/github_logo.png" alt="GitHub logo" className="w-[18px]"/>
-                    </a>
-                }
-                {
-                    proyecto.Vistas.preview && <a href={proyecto.url.preview}
-                    className="bg-blue-600 text-[13px] font-[800] px-3 gap-2 flex items-center h-8 rounded-lg hover:shadow-custom-white transition-all" target="_blank" rel="noopener noreferrer ">
-                        <span>Preview</span>
-                        <i className="fa-solid fa-up-right-from-square"></i>
-                    </a>
-                }
-
-            </div>
+  return (
+    <div className="w-[325PX] sm:w-[350PX] bg-[rgba(255,255,255,0.1)] flex flex-col items-center px-6 justify-around py-4 pb-3 rounded-lg mt-2 mb-[30px] md:mb-0 ">
+      <p className="font-[800] text-[20px] mb-2">
+        {proyecto.Nombre.primero} <span className="text-[#FF69B4]">{proyecto.Nombre.segundo}</span>
+      </p>
+      <div className="relative">
+        <div className="w-full h-[101%] flex flex-wrap items-end filtrado absolute rounded-lg">
+          <div className="h-fit py-[10px] px-3 w-full flex flex-wrap gap-1">
+            {proyecto.Tecnologias.map((item, index) => (
+              <div key={index} className="bg-gray-600 w-fit font-[800] text-[12px] py-1 px-2 rounded-lg text-nowrap ">
+                {item}
+              </div>
+            ))}
+          </div>
         </div>
-    )
-    }
+        <img src={proyecto.url.imagen} alt="" className="w-full rounded-lg" />
+      </div>
 
-export default Proyecto
+      <p className="text-[13px] mt-[20px] mb-[15px] font-[300] my-2 text-pretty">{proyecto.Descripcion}</p>
+      <div className="flex gap-5 w-full justify-end mb-2">
+        {proyecto.Vistas.github && (
+          <a
+            href={proyecto.url.github}
+            className="text-[13px] h-8 px-3 py-2 rounded-lg font-[800] bg-black flex items-center gap-2 hover:shadow-custom-white transition-all"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>Github</span>
+            <img src="img/github_logo.png" alt="GitHub logo" className="w-[18px]" />
+          </a>
+        )}
+        {proyecto.Vistas.preview && (
+          <a
+            href={proyecto.url.preview}
+            className="bg-blue-600 text-[13px] font-[800] px-3 gap-2 flex items-center h-8 rounded-lg hover:shadow-custom-white transition-all"
+            target="_blank"
+            rel="noopener noreferrer "
+          >
+            <span>Preview</span>
+            <i className="fa-solid fa-up-right-from-square"></i>
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Proyecto;
--- src/componentes/data.js
+++ src/componentes/data.js
@@ -1,98 +1,121 @@
 const dataSistemaDeInventarios = {
-    Nombre: {
-        primero: "Sistema de",
-        segundo: "inventarios"
-    },
-    Tecnologias: ["HTML", "CSS", "JavaScript", "GitHub"],
-    Descripcion: "Este proyecto es una aplicación web de escritorio, desarrollada con JavaScript, HTML y CSS vanilla, diseñada para gestionar inventarios de manera eficiente. La página permite a los usuarios registrar, editar y eliminar productos, así como visualizar el stock disponible. El diseño limpio y la lógica implementada aseguran un buen rendimiento en la gestión de datos de inventario.",
-    Vistas: {
-        github: true,
-        preview: true
-    },
-    url: {
-        github: "https://github.com/MarlonLupaca/Sistemas-de-inventarios",
-        preview: "https://marlonlupaca.github.io/Sistemas-de-inventarios/",
-        imagen: "img/home.png"
-    }
-}
+  Nombre: {
+    primero: 'Sistema de',
+    segundo: 'inventarios',
+  },
+  Tecnologias: ['HTML', 'CSS', 'JavaScript', 'GitHub'],
+  Descripcion:
+    'Este proyecto es una aplicación web de escritorio, desarrollada con JavaScript, HTML y CSS vanilla, diseñada para gestionar inventarios de manera eficiente. La página permite a los usuarios registrar, editar y eliminar productos, así como visualizar el stock disponible. El diseño limpio y la lógica implementada aseguran un buen rendimiento en la gestión de datos de inventario.',
+  Vistas: {
+    github: true,
+    preview: true,
+  },
+  url: {
+    github: 'https://github.com/MarlonLupaca/Sistemas-de-inventarios',
+    preview: 'https://marlonlupaca.github.io/Sistemas-de-inventarios/',
+    imagen: 'img/home.png',
+  },
+};
+const DataEcommerce = {
+  Nombre: {
+    primero: 'Sistema de',
+    segundo: 'ecommerce',
+  },
+  Tecnologias: ['NextJS', 'TailwindCSS', 'Spring Boot', 'React'],
+  Descripcion:
+    'Proyecto eCommerce FullStack con Next.js + React + Tailwind (frontend modular), backend en Spring Boot microservicios, comunicación WebSocket (chat en tiempo real), autenticación JWT, consumo de APIs con Axios, despliegue en Docker, y arquitectura desacoplada (Admin/Cliente).',
+  Vistas: {
+    github: true,
+    preview: false,
+  },
+  url: {
+    github: 'https://github.com/MarlonLupaca/FullStack',
+    preview: '#',
+    imagen: 'img/ecommerce.png',
+  },
+};
 
 const dataJavaEstructura = {
-    Nombre: {
-        primero: "Java, Estructura",
-        segundo: "de datos"
-    },
-    Tecnologias: ["Java", "CSV", "Estructura de datos"],
-    Descripcion: "El proyecto Acreedores del Estado, es un programa en Java diseñado para gestionar y administrar los datos financieros de acreedores, con un enfoque en totales en efectivo. Utiliza archivos CSV para almacenar la información y está implementado con diversas estructuras de datos avanzadas como árboles binarios, listas enlazadas, pilas, tablas hash y métodos de ordenamiento, optimizando así el manejo de grandes volúmenes de información.",
-    Vistas: {
-        github: true,
-        preview: false
-    },
-    url: {
-        github: "https://github.com/MarlonLupaca/Java_Acreedores_Estructura_de_datos",
-        preview: null,
-        imagen: "img/Estructura_de_datos.png"
-    }
-}
+  Nombre: {
+    primero: 'Java, Estructura',
+    segundo: 'de datos',
+  },
+  Tecnologias: ['Java', 'CSV', 'Estructura de datos'],
+  Descripcion:
+    'El proyecto Acreedores del Estado, es un programa en Java diseñado para gestionar y administrar los datos financieros de acreedores, con un enfoque en totales en efectivo. Utiliza archivos CSV para almacenar la información y está implementado con diversas estructuras de datos avanzadas como árboles binarios, listas enlazadas, pilas, tablas hash y métodos de ordenamiento, optimizando así el manejo de grandes volúmenes de información.',
+  Vistas: {
+    github: true,
+    preview: false,
+  },
+  url: {
+    github: 'https://github.com/MarlonLupaca/Java_Acreedores_Estructura_de_datos',
+    preview: null,
+    imagen: 'img/Estructura_de_datos.png',
+  },
+};
 
 const dataJavaFarmacia = {
-    Nombre: {
-        primero: "Java, Crud",
-        segundo: "Farmacia"
-    },
-    Tecnologias: ["Java", "MySQL", "Crud"],
-    Descripcion: "Este programa de administración de farmacia, desarrollado en Java con base de datos, permite gestionar todas las operaciones del negocio, aplicando funciones CRUD completas. Incluye control de inventario con alertas de vencimiento de productos, manejo de kardex, totales y diferenciación de roles entre administrador y trabajadores, garantizando una administración eficiente.",
-    Vistas: {
-        github: true,
-        preview: false
-    },
-    url: {
-        github: "https://github.com/MarlonLupaca/farmacia",
-        preview: null,
-        imagen: "img/Java_farmacia.png"
-    }
-}
+  Nombre: {
+    primero: 'Java, Crud',
+    segundo: 'Farmacia',
+  },
+  Tecnologias: ['Java', 'MySQL', 'Crud'],
+  Descripcion:
+    'Este programa de administración de farmacia, desarrollado en Java con base de datos, permite gestionar todas las operaciones del negocio, aplicando funciones CRUD completas. Incluye control de inventario con alertas de vencimiento de productos, manejo de kardex, totales y diferenciación de roles entre administrador y trabajadores, garantizando una administración eficiente.',
+  Vistas: {
+    github: true,
+    preview: false,
+  },
+  url: {
+    github: 'https://github.com/MarlonLupaca/farmacia',
+    preview: null,
+    imagen: 'img/Java_farmacia.png',
+  },
+};
 
 const dataJavaPasteleria = {
-    Nombre: {
-        primero: "Java, Crud",
-        segundo: "Pasteleria"
-    },
-    Tecnologias: ["Java", "SQLite", "Crud"],
-    Descripcion: "Este programa en Java utiliza SQLite para gestionar una pastelería a través de un CRUD básico que permite registrar, editar, eliminar y consultar datos. Fue el primer trabajo completo desarrollado de manera independiente, demostrando la capacidad de realizar todas las funciones esenciales para la administración de productos.",
-    Vistas: {
-        github: true,
-        preview: false
-    },
-    url: {
-        github: "https://github.com/MarlonLupaca/CrudJava",
-        preview: null,
-        imagen: "img/java_crud_basico.png"
-    }
-}
+  Nombre: {
+    primero: 'Java, Crud',
+    segundo: 'Pasteleria',
+  },
+  Tecnologias: ['Java', 'SQLite', 'Crud'],
+  Descripcion:
+    'Este programa en Java utiliza SQLite para gestionar una pastelería a través de un CRUD básico que permite registrar, editar, eliminar y consultar datos. Fue el primer trabajo completo desarrollado de manera independiente, demostrando la capacidad de realizar todas las funciones esenciales para la administración de productos.',
+  Vistas: {
+    github: true,
+    preview: false,
+  },
+  url: {
+    github: 'https://github.com/MarlonLupaca/CrudJava',
+    preview: null,
+    imagen: 'img/java_crud_basico.png',
+  },
+};
 
 const dataPorfolio = {
-    Nombre: {
-        primero: "Mi",
-        segundo: "portafolio"
-    },
-    Tecnologias: ["ReactJS", "Tailwindcss"],
-    Descripcion: "Este portafolio fue desarrollado utilizando React y Tailwind CSS, combinando una arquitectura moderna y un diseño minimalista y responsivo. Permite mostrar proyectos, habilidades y experiencia de manera interactiva y dinámica, con una interfaz fluida optimizada para diferentes dispositivos. Tailwind se utilizó para el estilo, ofreciendo una personalización rápida y eficiente del diseño.",
-    Vistas: {
-        github: true,
-        preview: false
-    },
-    url: {
-        github: "https://github.com/MarlonLupaca/Portfolio_dev",
-        preview: null,
-        imagen: "img/portafolio.png"
-    }
-}
+  Nombre: {
+    primero: 'Mi',
+    segundo: 'portafolio',
+  },
+  Tecnologias: ['ReactJS', 'Tailwindcss'],
+  Descripcion:
+    'Este portafolio fue desarrollado utilizando React y Tailwind CSS, combinando una arquitectura moderna y un diseño minimalista y responsivo. Permite mostrar proyectos, habilidades y experiencia de manera interactiva y dinámica, con una interfaz fluida optimizada para diferentes dispositivos. Tailwind se utilizó para el estilo, ofreciendo una personalización rápida y eficiente del diseño.',
+  Vistas: {
+    github: true,
+    preview: false,
+  },
+  url: {
+    github: 'https://github.com/MarlonLupaca/Portfolio_dev',
+    preview: null,
+    imagen: 'img/portafolio.png',
+  },
+};
 
-// Exportar los datos
 export {
-    dataSistemaDeInventarios,
-    dataJavaEstructura,
-    dataJavaFarmacia,
-    dataJavaPasteleria,
-    dataPorfolio
+  dataSistemaDeInventarios,
+  dataJavaEstructura,
+  dataJavaFarmacia,
+  dataJavaPasteleria,
+  dataPorfolio,
+  DataEcommerce,
 };
